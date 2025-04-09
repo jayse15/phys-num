dd00e02c3887a998a28d8a9692fc55a02fa396ac
Jason Grieder <jason@grieder.org>
2025-04-09 18:29:59 +0200

    avances

--- Ex4_2025_student/plotter.py
+++ Ex4_2025_student/plotter.py
@@ -9,7 +9,11 @@ plt.rcParams.update({
     'text.usetex': True,               # Use LaTeX for all text rendering
     'font.family': 'serif',            # Set font family to serif
     'font.serif': ['Computer Modern'], # Use Computer Modern
-    'figure.dpi': 150,                 # DPI for displaying figures
+    'figure.dpi': 250,                 # DPI for displaying figures
+    'font.size': 16,
+    'lines.linewidth':2,
+    'lines.markersize':5,
+    'axes.formatter.limits':(-3, 3)
 })
 
 # Parameters
@@ -54,31 +58,67 @@ def to_latex_sci(num, precision=2):
 
     return rf'{sign}{mantissa:.{precision}f}\cdot 10^{{{exponent}}}'
 
+rho0 = parameters['rho0']
+VR = parameters['VR']
+r1 = parameters['r1']
+R = parameters['R']
 
-nsteps = np.array([ 30e3, 40e3, 50e3, 60e3, 70e3, 80e3, 100e3, 200e3, 300e3])
-epsilon = np.array([1e-3, 1e-2, 1e-1, 10, 100, 1e3, 1e4, 1e5])
+def phi_0(r):
+    return (R**2-r**2)/4
+
+def E_0(r):
+    return r/2
+
+N = np.array([5, 6, 8, 10, 15, 20, 30, 40, 50, 60, 70, 100])
 outputs = []
 datas=[]
-nsimul = len(nsteps)
+nsimul = len(N)
 
 for i in range(nsimul):
-    output_file = f"data/nsteps={nsteps[i]}.out"
+    output_file = f"data/N={N[i]}"
     outputs.append(output_file)
-    cmd = f"{repertoire}{executable} {input_filename} nsteps={nsteps[i]} output={output_file}"
+    cmd = f"{repertoire}{executable} {input_filename} N1={N[i]} N2={N[i]} output={output_file}"
     print(cmd)
     subprocess.run(cmd, shell=True)
     print('Done.')
 
-
+E, D, Phi = [], [], []
+conv_phi = []
 for i in range(nsimul):  # Iterate through the results of all simulations
-    data = np.loadtxt(outputs[i])  # Load the output file of the i-th simulation
+    e = np.loadtxt(outputs[i]+'_E.out')  # Load the output file of the i-th simulation
+    d = np.loadtxt(outputs[i]+'_D.out')  # Load the output file of the i-th simulation
+    p = np.loadtxt(outputs[i]+'_phi.out')  # Load the output file of the i-th simulation
+    E.append(e)
+    D.append(d)
+    Phi.append(p)
+    conv_phi.append(p[0, 1])
 
-    t_n = data[:, 0]
-    xx = data[-1, 3]
-    yy = data[-1, 4]
-    En = data[-1, 5]
-    v = np.sqrt(data[:, 1]**2 + data[:, 2]**2)
-    r = np.sqrt(data[:, 3]**2 + data[:, 4]**2)
+r = E[1][:, 0]
+plt.figure()
+plt.plot(r, E[1][:, 1], 'o',  c='orange', label='Data', markerfacecolor='none')
+plt.plot(r, E_0(r), 'k--', label='True')
+plt.xlabel(r'$r$[m]')
+plt.ylabel(r'$E(r)$ [V/m]')
+plt.legend()
+plt.show()
 
+plt.figure()
+plt.plot(D[1][:, 0], D[1][:, 1], 'go', markerfacecolor='none')
+plt.xlabel(r'$r$[m]')
+plt.ylabel(r'$D(r)$ [C/m$^2$]')
+plt.show()
 
-    datas.append(data)
+r = Phi[1][:, 0]
+plt.figure()
+plt.plot(r, Phi[1][:, 1], 'ro', label='Data', markerfacecolor='none')
+plt.plot(r, phi_0(r), 'k--', label='True')
+plt.xlabel(r'$r$[m]')
+plt.ylabel(r'$\phi(r)$ [V]')
+plt.legend()
+plt.show()
+
+plt.figure()
+plt.plot((1/N)**2, conv_phi, 'k+-')
+plt.xlabel(r'$1/N^2_1(=1/N^2_2)$')
+plt.ylabel(r'$\phi(0)$ [V]')
+plt.show()
